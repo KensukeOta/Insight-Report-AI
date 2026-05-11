042b924423783908b3761ab030adf41497c0ede9
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2026-05-11 19:17:53 +0900

    テスト

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}a</body>
     </html>
   );
 }
